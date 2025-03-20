b3f2ee8cbcc8f78d4c8fce120b982e63cdc22998
OdinaryStar <1078138367@qq.com>
2025-03-21 00:58:21 +0800

    second commit

---
+++ git_update.py
@@ -0,0 +1,12 @@
+# git提交代码到远程仓库
+# 1. git add .
+print("git add .")
+print("git add 文件名")
+# 2. git commit -m "update"
+print("git commit -m 'update'")
+# 3. git pull(更新远程仓库代码)
+print("git pull")
+# 4. git push
+print("git push")
+
+print("git提交代码到远程仓库")
